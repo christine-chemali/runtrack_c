ff5a88d893af6841104f7612ea31d04a1d163205
christine chemali <christine.chemali@laplateforme.io>
2025-03-31 14:23:11 +0200

    Runtrack C Day 1 Job 1/2 my_putchar

---
+++ JOUR01/JOB01/my_putchar.c
@@ -0,0 +1,23 @@
+/*
+Job 01
+Vous devez implémenter deux fonctions, my_putchar (dans un fichier my_putchar.c) et
+my_putstr (dans un fichier my_putstr.c)
+La fonction my_putchar prend un caractère en paramètre, et l’écrit sur la sortie
+standard. La fonction my_putstr prend une chaîne de caractères en paramètre, et l’écrit
+sur la sortie standard, en utilisant la fonction my_putchar.
+*/
+
+#include <stdio.h>
+
+void my_putchar(char c) {
+  putchar(c);
+} 
+int main(){
+  my_putchar('H');
+  my_putchar('E');
+  my_putchar('L');
+  my_putchar('L');
+  my_putchar('O');
+  my_putchar('!');
+  return 0;
+}
